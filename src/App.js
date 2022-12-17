9e1faa68ee94a23145209f9f6a7c04a69c89b09a
Kemal Burak Ecir <114359416+kemalburakecir@users.noreply.github.com>
2022-12-17 11:10:15 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { useState } from "react"
+
+
+export function App() {
+
+  const [count,setCount] = useState(0)
+
+ function handleClick () {
+    const newCount = count+1
+    setCount(newCount)
+  }
+
+  return ( 
+        <div>
+          Hello World
+            <div>
+         <button
+                onClick={ handleClick }
+                >
+                  Click to increase
+                  </button>
+         <div>{count}</div>
+            </div>
+  </div>
+  )
+}
+
